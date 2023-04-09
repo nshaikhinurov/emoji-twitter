b8c3ba9ddc2e107dd229704a87000fcc9a157ef2
Nail Shaikhinurov <n.shaikhinurov@gmail.com>
2023-04-09 16:54:29 +0800

    Styled up

--- next.config.mjs
+++ next.config.mjs
@@ -18,5 +18,16 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 export default config;
